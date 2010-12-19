f797a1e8fe07d07c224e8ec5c1ab9176c966e102
Yangmun <yangmungi@gmail.com>
2010-12-18 21:40:34 -0800

    Added half-sorting search results.

--- index.php
+++ index.php
@@ -2,124 +2,15 @@
 <head>
     <title> Space Port v1.00 </title>
     <link type="text/css" href="css/custom-theme/jquery-ui-1.8.7.custom.css" rel="stylesheet" />
+    <link type="text/css" href="mpd-spacedock.css" rel="stylesheet" />
 </head>
 <body>
-
-<style type="text/css">
-
-/** Global **/
-body {
-    min-width: 1024px;
-}
-
-/** Play Controls **/
-#play-controls {
-    position: fixed;
-    padding: 4px;
-    width: 98%;
-    height: auto;
-    background-color: #FFFFFF;
-}
-
-.mod-title {
-    text-align: center;
-    margin: 2px;
-    padding: 0px 8px;
-    float: left;
-}
-
-.play-icons {
-    padding: 4px;
-    margin: 2px;
-    float: left;
-}
-
-#voluprogwrap {
-    float: left;
-    margin: 8px;
-    width: 40px;
-}
-
-#playprogwrap {
-    width: 400px;
-    float: left;
-    margin: 8px 14px;
-}
-
-#playprogtext {
-    margin: 4px;
-    float: left;
-}
-
-#songlist {
-    top: 50px;
-    width: 50%;
-    left: 12px;
-    position: absolute;
-}
-
-#playlistcowrap {
-    clear: both;
-}
-
-.playlistco {
-    float: right;
-    padding: 4px;
-    margin: 2px 24px 2px 0px;
-}
-
-.song-item {
-    float: left;
-    width: 85%;
-    padding: 2px;
-    margin: 2px;
-}
-
-#searchwrap {
-    width: 100%;
-}
-
-#searchres {
-    top: 50px;
-    right: 0px;
-    width: 50%;
-    position: absolute;
-}
-
-.search-reit {
-    padding: 2px;
-    margin: 2px 2px;
-    width: 95%;
-    float: left;
-}
-
-.search-icons {
-    float: left;
-    padding: 4px;
-    margin: 2px 2px;
-}
-
-.search-item {
-
-}
-
-#searchfield {
-    float: left;
-}
-
-.ui-selected {
-    background: #DB4865 url(http://localhost/mpd/css/custom-theme/images/ui-bg_glass_40_db4865_1x400.png) repeat-x 50% 50%;
-}
-
-.no-display {
-    display: none;
-}
-
-</style>
 
 <?php
 
 $playbtn = array();
+$sephelp = 0;
+
 $playbtn['prev']['title'] = 'Previous Song';
 $playbtn['prev']['icon']  = 'seek-first';
 
@@ -132,11 +23,34 @@ $playbtn['play']['icon']  = 'play';
 $playbtn['next']['title'] = 'Next Song';
 $playbtn['next']['icon']  = 'seek-end';
 
+$sepstr = 'sep' . $sephelp;
+$playbtn[$sepstr]['title'] = '';
+$playbtn[$sepstr]['icon']  = 'grip-solid-vertical';
+$playbtn[$sepstr]['nullvoid'] = TRUE;
+$sephelp++;
+
 $playbtn['psclear']['title'] = 'Clear Playlist';
 $playbtn['psclear']['icon']  = 'trash';
 
-//$playbtn['refresh']['title'] = 'Quick Refresh';
-//$playbtn['refresh']['icon']  = 'arrowrefresh-1-w';
+$sepstr = 'sep' . $sephelp;
+$playbtn[$sepstr]['title'] = '';
+$playbtn[$sepstr]['icon']  = 'grip-solid-vertical';
+$playbtn[$sepstr]['nullvoid'] = TRUE;
+$sephelp++;
+
+$playbtn['plmode']['title'] = 'Play Until End of this Song';
+$playbtn['plmode']['icon']  = 'arrowstop-1-e';
+
+$sepstr = 'sep' . $sephelp;
+$playbtn[$sepstr]['title'] = '';
+$playbtn[$sepstr]['icon']  = 'grip-solid-vertical';
+$playbtn[$sepstr]['nullvoid'] = TRUE;
+$sephelp++;
+
+/**
+$playbtn['refresh']['title'] = 'Quick Refresh';
+$playbtn['refresh']['icon']  = 'arrowrefresh-1-w';
+**/
 
 $conbtn = array();
 $conbtn['play']['icon']  = 'play'; 
@@ -146,6 +60,7 @@ $conbtn['remove']['icon']  = 'circle-close';
 $conbtn['remove']['title'] = 'Remove From Playlist';
 
 ?>
+
 <script type="text/javascript" src="jquery.js"></script>
 <script type="text/javascript" src="jquery.ui.js"></script>
 <script type="text/javascript">
@@ -157,12 +72,19 @@ var playListCurr;
 var volPrev;
 
 var currState;
+var currRepeat;
+var currSingle;
+
+var searchCache;
 
 /** Initial Assigns and Event Listeners **/
 $(document).ready( function() {
     /** Play Controls **/
 <?php
     foreach ($playbtn as $id => $btn) {
+        if (isset($btn['nullvoid'])) {
+            continue;
+        }
         echo "\t$('#" . $id . "').click( cmd_$id );";
     }
 ?>
@@ -171,13 +93,15 @@ $(document).ready( function() {
     /** Search Controls **/
     $('#searchbutton').click( function() {
         var searching = $('#searchfield').val();
-        sendMPD('search', 'artist ' + searching, searchResponse, 
-            true, false);
-    });
 
-    $('#resetsearch').click( function() {
-        $('#searchfield').val('');
-        searchResponse('[]');
+        clearSearches();
+        sendMPD('search', 'artist ' + searching, searchResponse, 
+            false, false);
+        sendMPD('search', 'album ' + searching, searchResponse, 
+            false, false);
+        sendMPD('search', 'title ' + searching, searchResponse, 
+            false, false);
+        publishSearches();
     });
 
     $('#addallsongs').click( function () {
@@ -195,14 +119,111 @@ $(document).ready( function() {
 
     assignHover('.search-icons');
 
-    /** Entire Play Controls **/
-    //$('#play-controls').draggable();
-    
     /** Initial Update **/
     updateAddButtons();
     updateState();
-    searchResponse('[]');
 });
+
+function clearSearches() {
+    searchCache = new Array();
+    $('#searchres').empty();
+}
+
+function publishSearches() {
+    if (searchCache == undefined || searchCache == null) {
+        updateDebug("No search results");
+        return false;
+    }
+
+    // Shmidty?
+
+    var sortSC = searchCache.sort(
+        function(a, b) {
+            // If we do all pass, anthropology!
+            var sOrder = ["artist", "year", "disc", "track"];
+            return searchHelper(a, b, sOrder.shift(), sOrder);
+        });
+
+    var sSong;
+
+    var curAlbum = '';
+    var preAlbum = '';
+
+    var sDisc = '';
+    var sYear = '';
+
+    for (var i in sortSC) {
+        sSong = searchCache[i];
+
+        if (sSong.album == undefined) {
+            curAlbum = 'No Album';
+        } else {
+            if (sSong.disc != undefined) {
+                sDisc = ' (Disc ' + sSong.disc + ')';
+            } else {
+                sDisc = '';
+            }
+
+            if (sSong.date != undefined) {
+                sYear = sSong.date + ' ';
+            } else {
+                sYear = '';
+            }
+
+            curAlbum = sYear + sSong.album + sDisc;
+        }
+
+        if (preAlbum != curAlbum) {
+            
+            $('#searchres').append(
+                  '<div class="search-item">'
+                + '<div class="ui-state-default ui-corner-all '
+                + 'album-reit album-reit-name">' 
+                +  curAlbum + '</div>'
+                + '</div>'
+            );
+
+            preAlbum = curAlbum;
+        }
+        addSearchResult(sSong, i);
+    }
+
+    $('#searchres').selectable({
+        filter: '.search-reit',
+        stop: updateAddButtons
+    });
+
+    $(this).fadeIn(300, function() {
+        $('.search-all-icons').fadeIn();
+        updateAddButtons();
+    });
+}
+
+function searchHelper(a, b, field, nextarr) {
+    if (eval('a.' + field) == undefined) {
+        if (eval('b.' + field) == undefined) {
+            eval('a.' + field + ' = 0');
+            eval('b.' + field + ' = 0');
+        } else {
+            eval('a.' + field + ' = b.' + field);
+        }
+    }
+
+    if (eval('b.' + field) == undefined) {
+        eval('b.' + field + ' = a.' + field);
+    }
+
+    if (eval('a.' + field + ' > b.' + field)) {
+        return 1;
+    } else if (eval('a.' + field + ' < b.' + field)) {
+        return -1;
+    } else {
+        if (nextarr.length > 0) {
+            var next = nextarr.shift();
+            searchHelper(a, b, next, nextarr);
+        }
+    }
+}
 
 function playListAddSongs(jQCore) {
     $(jQCore).each( function (n, e) {
@@ -215,34 +236,17 @@ function playListAddSongs(jQCore) {
 }
 
 function searchResponse(args) {
-    var fadeTime = 300;
-    $('.search-all-icons').fadeOut();
-    $('#searchres').fadeOut(fadeTime, function () {
-        $(this).empty();
+    if (args == '[]') {
+        return false;
+    }
+    
+    var sres = JSON.parse(args);
 
-        if (args == '[]') {
-            return;
-        }
+    for (var i in sres) {
+        searchCache.push(sres[i]);
+    }
 
-        var sres = JSON.parse(args);
-        var sSong;
-
-        for (var i in sres) {
-            sSong = sres[i];
-
-            addSearchResult(sSong, i);
-        }
-
-        $('#searchres').selectable({
-            filter: '.search-reit',
-            stop: updateAddButtons
-        });
-
-        $(this).fadeIn(fadeTime, function() {
-            $('.search-all-icons').fadeIn();
-            updateAddButtons();
-        });
-    });
+    return false;
 }
 
 function updateAddButtons() {
@@ -266,13 +270,20 @@ function addSearchResult(song, searchId) {
     if (song.artist == undefined || song.title == undefined) {
         songDisp = song.file;
     } else {
-        songDisp = 
-              song.artist + ' - ' + song.album 
-            + ' (' + song.track + '): ' + song.title;
+        var trackStr = '';
+        if (song.track != undefined) {
+            trackStr = '(' + song.track + ') ';
+        }
+
+        songDisp = trackStr + song.artist + ' - ' + song.title;
     }
 
     $('#searchres').append(
               '<div class="search-item">'
+            + '<div class="ui-state-default ui-corner-all '
+            + 'album-reit album-reit-tl">'
+            + '<span class="ui-icon ui-icon-bullet"></span>'
+            + '</div>'
             + '<div class="ui-state-default ui-corner-all search-reit">' 
             + songDisp + '</div>'
             + '<div class="no-display songinfo">'
@@ -345,6 +356,31 @@ function cmd_psclear() {
     sendMPD('clear');
 }
 
+function cmd_plmode() {
+    if (currRepeat == undefined || currSingle == undefined) {
+        updateState();          
+    }
+
+    if (currSingle == 1) {
+        if (currRepeat == 1) {
+            currSingle = 0;
+            currRepeat = 0;
+        } else {
+            currRepeat = 1;
+        }
+    } else {
+        if (currRepeat == 1) {
+            currSingle = 1;
+            currRepeat = 0;
+        } else {
+            currRepeat = 1;
+        }
+    }
+
+    sendMPD('repeat', currRepeat, function() {} );
+    sendMPD('single', currSingle);
+}
+
 function cmd_remove(arg) {
     var playArg = currentTargetExtract(arg.currentTarget.id, playListCurr);
 
@@ -361,15 +397,15 @@ function updateDebug(data) {
     $('#debuggah').append(data + "\n");
 }
 
-/** Status Functions **/
-function updateState() {
-    sendMPD('status', '', updateStatuses); 
-}
-
 function parseMPDResults(inputs) {
     var jsonObj = JSON.parse(inputs);
 
     return jsonObj;
+}
+
+/** Status Functions **/
+function updateState() {
+    sendMPD('status', '', updateStatuses); 
 }
 
 function updateStatuses(arg) {
@@ -407,7 +443,7 @@ function updatePlaylist(arg) {
             + songInfo[i]['title'];
         $('#songlist').append(
               '<div id="'+ playListId + '" '
-            + 'class="float-left">' 
+            + '>' 
             + '<div class="ui-state-default ui-corner-all song-item">'
             + songInfoString 
             + '</div>'
@@ -574,6 +610,41 @@ function toMinutes(splits) {
             remMin + ':' + remSec];
 }
 
+function updateState_repeat(arg) {
+    currRepeat = arg;
+
+    if (currSingle != undefined) {
+        updateRepeatButton();
+    }
+}
+
+function updateState_single(arg) {
+    currSingle = arg;
+
+    if (currRepeat != undefined) {
+        updateRepeatButton();
+    }
+}
+
+function updateRepeatButton() {
+    var iconstr = 'arrowthickstop-1-e';
+    var icontit = 'Play to End of Playlist';
+
+    if (currSingle == 1 && currRepeat == 1) {
+        iconstr = 'arrowreturn-1-w';
+        icontit = 'Repeat Song';
+    } else if (currSingle == 1) {
+        iconstr = 'arrowstop-1-e';
+        icontit = 'Play to End of Song';
+    } else if (currRepeat == 1) {
+        iconstr = 'arrowreturnthick-1-w';
+        icontit = 'Repeat Playlist';
+    } 
+
+    $('#plmode span').removeClass().addClass('ui-icon ui-icon-' + iconstr);
+    $('#plmode').attr('title', icontit);
+}
+
 /** Background Helper Services **/
 function sendMPD(cmd, args, customcallback, asyncOpt, forceQuote) {
     if (cmd == undefined) {
@@ -618,17 +689,18 @@ function sendMPD(cmd, args, customcallback, asyncOpt, forceQuote) {
 </div>
 
 <div id="play-controls" class="ui-widget ui-widget-content ui-corner-all">
-<!--
-    <div class="ui-widget-header ui-corner-all mod-title">
-        Player Controls
-    </div>
--->
 
 <?php
 
 foreach ($playbtn as $id => $btn) {
-    echo "\n" . build_div_button($id, $btn['icon'], 
+    if (isset($btn['nullvoid'])) {
+        echo "\n" . '<span class="ui-icon ui-icon-' . $btn['icon']
+            . ' separator"></span>';
+
+    } else {
+        echo "\n" . build_div_button($id, $btn['icon'], 
             'play-icons', $btn['title']) . "\n";
+    }
 }
 
 ?>
@@ -655,30 +727,17 @@ $searchbtn['addallsongs']['title'] = 'Add All Songs';
 $searchbtn['addallsongs']['icon']  = 'circle-plus';
 $searchbtn['addallsongs']['class'] = 'search-all-icons';
 
-/*
-$searchbtn['resetsearch']['title'] = 'Remove Search Results';
-$searchbtn['resetsearch']['icon']  = 'trash';
-$searchbtn['resetsearch']['class'] = 'search-all-icons';
-*/
-
 $searchbtn['addsinglesong']['title'] = 'Add Selected Song(s)';
 $searchbtn['addsinglesong']['icon']  = 'plus';
 $searchbtn['addsinglesong']['class'] = 'search-sel-icons';
 
-/**
-$searchbtn['deselsearch']['title'] = 'Deselect Songs';
-$searchbtn['deselsearch']['icon']  = 'close';
-$searchbtn['deselsearch']['class'] = 'search-sel-icons';
-**/
-
 foreach ($searchbtn as $id => $btn) {
     echo build_div_button($id, $btn['icon'], 
-            'search-icons ' . $btn['class'], $btn['title'])
-        . "\n";
+            'search-icons ' . $btn['class'], $btn['title']) . "\n";
 }
 
 ?>
-
+    </div>
 </div>
 
 <pre id="debuggah"></pre>
